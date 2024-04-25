7b153d9e292dc503ac2f2bb235f4a39ebe787edf
kentigerc <157593216+kentigerc@users.noreply.github.com>
2024-04-25 09:03:28 +0300

    first commit

---
+++ Solution.sql
@@ -0,0 +1,19 @@
+-- use 8week
+
+#inserting new values into the customers table from 8week database
+-- INSERT INTO `8week`.`customers` (`customer_id`, `name`, `city`) 
+-- VALUES ('4', 'Alice Smith', 'New York City');
+
+#updating the name 'New Yory City' to 'New York City'
+-- UPDATE `8week`.`customers` 
+-- SET `city` = 'New York City' 
+-- WHERE (`customer_id` = '2');
+
+#finding all customers who live in Chicago
+-- SELECT * from customers
+-- WHERE CITY = "chicago";
+
+#deleting customer with customer ID '1' from the Database
+-- DELETE FROM customers
+-- Where customer_id = 1;
+
